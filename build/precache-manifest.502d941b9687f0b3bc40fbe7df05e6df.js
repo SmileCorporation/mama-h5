093da37dc03b7149726476e19db0eb468f31e5e9
曹信坤 <xinkuncao@hand-china.com>
2021-06-04 15:06:35 +0800

    [NEW]新增

---
+++ build/precache-manifest.502d941b9687f0b3bc40fbe7df05e6df.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66565b1f97ff93acd8956b2a54682142",
+    "url": "index.html"
+  },
+  {
+    "revision": "646d471953902281dfe97cce58eb8715",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "eca605bab7f583bd3ad9",
+    "url": "static/css/aboutMe.af97be66.css"
+  },
+  {
+    "revision": "cbeebba05e49de022dd2",
+    "url": "static/css/addSource.aea3b9f6.css"
+  },
+  {
+    "revision": "40b9cc5c33bc2d3267f7",
+    "url": "static/css/admin.c977c88d.css"
+  },
+  {
+    "revision": "9f16b288ec570749cb68",
+    "url": "static/css/adminIndex.1c5339ad.css"
+  },
+  {
+    "revision": "885f70ef844854b36eed",
+    "url": "static/css/app.c819b619.css"
+  },
+  {
+    "revision": "4e7b73b8c3345601688c",
+    "url": "static/css/chunk-vendors.6f37a540.css"
+  },
+  {
+    "revision": "4eb70049da5830ed0090",
+    "url": "static/css/myInfo.9e2ac674.css"
+  },
+  {
+    "revision": "9f9754d36bb095764dd0",
+    "url": "static/css/sourceManage.14c04334.css"
+  },
+  {
+    "revision": "aaa908b170365bd35e78",
+    "url": "static/css/toDo.a5b2c010.css"
+  },
+  {
+    "revision": "b065c379e7c921e064db415d01baf880",
+    "url": "static/img/bg1.b065c379.png"
+  },
+  {
+    "revision": "40896522a7de50e26f5b905c1dca4489",
+    "url": "static/img/error.40896522.gif"
+  },
+  {
+    "revision": "1df8df97f9f8920c2e04d2a8b6888c5e",
+    "url": "static/img/loading.1df8df97.gif"
+  },
+  {
+    "revision": "eca605bab7f583bd3ad9",
+    "url": "static/js/aboutMe.9e90428c.js"
+  },
+  {
+    "revision": "cbeebba05e49de022dd2",
+    "url": "static/js/addSource.def4ea47.js"
+  },
+  {
+    "revision": "40b9cc5c33bc2d3267f7",
+    "url": "static/js/admin.36d63464.js"
+  },
+  {
+    "revision": "9f16b288ec570749cb68",
+    "url": "static/js/adminIndex.1837c63d.js"
+  },
+  {
+    "revision": "885f70ef844854b36eed",
+    "url": "static/js/app.d707d3f7.js"
+  },
+  {
+    "revision": "4e7b73b8c3345601688c",
+    "url": "static/js/chunk-vendors.2640a1cd.js"
+  },
+  {
+    "revision": "0b22368b045ae4050a38",
+    "url": "static/js/mobileMapDisplay.8de5574d.js"
+  },
+  {
+    "revision": "4eb70049da5830ed0090",
+    "url": "static/js/myInfo.a2d03f6c.js"
+  },
+  {
+    "revision": "9f9754d36bb095764dd0",
+    "url": "static/js/sourceManage.b2a32a22.js"
+  },
+  {
+    "revision": "aaa908b170365bd35e78",
+    "url": "static/js/toDo.ac528441.js"
+  }
+]);
